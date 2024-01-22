6526a27574f0288ca5929d570e544cc65ca25b62
Asaba William <asabawilliamdk@yahoo.com>
2024-01-22 11:38:55 +0300

    quiz app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quiz-App-PHP
+
+<h3>Quiz Application made with PHP and Front-End Technologies. </h3> 
+<h4>Concepts and Methods implemented in this project : </h4> 
+
+- Data Fetching from forms to PHP Scripts.
+- PHP Form Validation
+- Making POST Requests and redirecting to different routes.
+- Reusing PHP scripts to embed them as template to other pages.
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div class="header">
+        <ul class="head-items">
+            <li> <a id="title" href="index.php"> Quizophile </a></li>
+            <li id="first-item"><a href="play.php">Play</a> </li>
+        </ul>
+    </div>
+</body>
+
+</html>
